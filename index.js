7800ef7e6df57097ae7d1621fe62f5a6ddf051a7
SeanWelshBrown <seanwelshbrown@gmail.com>
2020-01-26 21:10:52 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(arrayOfStrings, eventName) {
+  let messages = []
+  for (let i = 0; i < arrayOfStrings.length; i++) {
+    messages.push(`Thank you, ${arrayOfStrings[i]}, for the wonderful ${eventName} gift!`);
+  }
+  return messages;
+}
+
+function countDown(posInteger) {
+  let count = posInteger;
+  while (count >= 0) {
+    console.log(count--);
+  }
+}
